00c841a0c61f5f53b1dee2892f48769af168b73c
Aakashdeep Maurya <126263842+aakashmaurya122@users.noreply.github.com>
2023-02-25 09:34:55 +0530

    Add files via upload

---
+++ repo/index.js
@@ -0,0 +1,48 @@
+function getData(){
+    var request = new XMLHttpRequest()
+    request.open("get","https://restcountries.com/v3.1/name/bharat")
+    request.send()
+
+    request.addEventListener("load",()=>{
+        var [data] = JSON.parse(request.responseText)
+        document.getElementById("name").innerHTML =  data.name.official
+        document.getElementById("flags").src =  data.flags.svg
+        document.getElementById("capital").innerHTML =  data.capital[0]
+        document.getElementById("population").innerHTML =  data.population
+        document.getElementById("area").innerHTML =  data.area
+        document.getElementById("region").innerHTML =  data.region
+        document.getElementById("subregion").innerHTML =  data.subregion
+        document.getElementById("maps").href =  data.maps.googleMaps
+        document.getElementById("borders").innerHTML =  data.borders
+        document.getElementById("timezones").innerHTML =  data.timezones
+        document.getElementById("independent").innerHTML =  data.independent
+        document.getElementById("unMember").innerHTML =  data.unMember
+        document.getElementById("landlocked").innerHTML =  data.landlocked
+        document.getElementById("startOfWeek").innerHTML =  data.startOfWeek
+    })
+}
+getData()
+function fun(){
+    var name = document.getElementById("search").value
+    var request = new XMLHttpRequest()
+    request.open("get","https://restcountries.com/v3.1/name/"+name)
+    request.send()
+
+    request.addEventListener("load",()=>{
+        var [data] = JSON.parse(request.responseText)
+        document.getElementById("name").innerHTML =  data.name.official
+        document.getElementById("flags").src =  data.flags.svg
+        document.getElementById("capital").innerHTML =  data.capital[0]
+        document.getElementById("population").innerHTML =  data.population
+        document.getElementById("area").innerHTML =  data.area
+        document.getElementById("region").innerHTML =  data.region
+        document.getElementById("subregion").innerHTML =  data.subregion
+        document.getElementById("maps").href =  data.maps.googleMaps
+        document.getElementById("borders").innerHTML =  data.borders
+        document.getElementById("timezones").innerHTML =  data.timezones
+        document.getElementById("independent").innerHTML =  data.independent
+        document.getElementById("unMember").innerHTML =  data.unMember
+        document.getElementById("landlocked").innerHTML =  data.landlocked
+        document.getElementById("startOfWeek").innerHTML =  data.startOfWeek
+    })
+}
